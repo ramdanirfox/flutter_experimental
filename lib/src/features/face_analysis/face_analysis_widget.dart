125dd2c7ca3fa729cef75fa78a94859d939720dc
iramdan461 <iramdan461@gmail.com>
2025-03-14 16:22:16 +0700

    fix: face analysis (WIP 2)

--- lib/src/features/face_analysis/face_analysis_widget.dart
+++ lib/src/features/face_analysis/face_analysis_widget.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:face_verify/face_verify.dart';
 import 'package:camera/camera.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
 
 class FaceAnalysisDialog extends StatelessWidget {
-  const FaceAnalysisDialog({Key? key}) : super(key: key);
+  const FaceAnalysisDialog({super.key});
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: const Text('Fraud Analysis'),
+      title: const Text('Face Analysis'),
       content: const Text('Press Button to start the process.'),
       actions: <Widget>[
         TextButton(
@@ -18,16 +22,32 @@ class FaceAnalysisDialog extends StatelessWidget {
           onPressed: () async {
             WidgetsFlutterBinding.ensureInitialized();
             final cameras = await availableCameras();
-            final cameraDescription = cameras.first;
+            final cameraDescription = cameras.elementAt(1);
+
+            final String user1ImagePath =
+                await copyAssetToFile('assets/images/user1.jpg');
+            print(user1ImagePath);
             final List<UserModel> users = await registerUsers(
               registerUserInputs: [
                 RegisterUserInputModel(
-                    name: 'User1', imagePath: 'path/to/image1.jpg'),
-                RegisterUserInputModel(
-                    name: 'User2', imagePath: 'path/to/image2.jpg'),
+                    name: 'User1', imagePath: user1ImagePath),
+                // RegisterUserInputModel(
+                //     name: 'User2', imagePath: 'path/to/image2.jpg'),
               ],
               cameraDescription: cameraDescription,
-            );
+            );            
+            Set<UserModel>? recognizedUsers = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetectionView(
+                    users: users,
+                    cameraDescription: cameraDescription,
+                    // frameSkipCount: 10,
+                  ),
+                ));
+            if (recognizedUsers != null && recognizedUsers.isNotEmpty) {
+              print('recognized users : $recognizedUsers');
+            }
             ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(content: Text('Analysis Done $cameraDescription')));
           },
@@ -35,5 +55,16 @@ class FaceAnalysisDialog extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  Future<String> copyAssetToFile(String assetPath) async {
+    final byteData = await rootBundle.load(assetPath);
+    final buffer = byteData.buffer.asUint8List();
+
+    final tempDir = await getTemporaryDirectory();
+    final file = File('${tempDir.path}/${path.basename(assetPath)}');
+
+    await file.writeAsBytes(buffer);
+    return file.path;
   }
 }
